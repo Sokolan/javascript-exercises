09b67f2fb7fc7b6178d826c83314b1013de291a1
Sokolan <valerysokolan@gmail.com>
2023-03-19 18:31:29 +0200

    10_fibonacci , 11_getTheTitles, 12_findTheOldest - done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n <= 0){
+        return "OOPS";
+    }
+    if(n === 1){
+        return 1;
+    }
+    let a = 1;
+    let b = 1;
+    for(let i = 2 ; i < n ; ++i){
+        [a,b] = [a + b, a];
+    }
+    return a;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+function getAge(death,birth){
+    if(!death){
+        death = new Date().getFullYear()
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(arr) {
+    if(!arr.length){
+        return "OOPS";
+    }
+    return arr.reduce((oldest, curr) =>{
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currAge = getAge(curr.yearOfDeath, curr.yearOfBirth); 
+        return oldestAge > currAge ? oldest : curr;
+    });
 };
 
 // Do not edit below this line
