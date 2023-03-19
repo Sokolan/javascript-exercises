3f76cc140888ff9c068cf94753e20b43ac09589f
Sokolan <valerysokolan@gmail.com>
2023-03-19 17:10:56 +0200

    09_palindromes - done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strLower = str.toLowerCase().replace(/[^a-z]/g, "");
+    return strLower == strLower.split("").reverse().join("");
 };
 
 // Do not edit below this line
